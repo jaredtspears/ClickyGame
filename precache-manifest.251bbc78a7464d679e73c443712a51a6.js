f1d57192d638d2c4f3b6ca9952be02f483a39717
Ha1f_Deaf <jaredtspears@gmail.com>
2018-12-19 19:42:28 -0500

    Updates

---
+++ precache-manifest.251bbc78a7464d679e73c443712a51a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c315db57844be2c86c5",
+    "url": "/ClickyGame/static/css/main.8f403837.chunk.css"
+  },
+  {
+    "revision": "1c315db57844be2c86c5",
+    "url": "/ClickyGame/static/js/main.1c315db5.chunk.js"
+  },
+  {
+    "revision": "f6d5796e6df2dc452017",
+    "url": "/ClickyGame/static/css/1.a460cab1.chunk.css"
+  },
+  {
+    "revision": "f6d5796e6df2dc452017",
+    "url": "/ClickyGame/static/js/1.f6d5796e.chunk.js"
+  },
+  {
+    "revision": "59843ae1c2ed89599fd0",
+    "url": "/ClickyGame/static/js/runtime~main.59843ae1.js"
+  },
+  {
+    "revision": "4be95681dfbb2dd5469c9485a5ea4aed",
+    "url": "/ClickyGame/index.html"
+  }
+];
